0bfe1503d8a49521ce6d24e4520979856d43d702
Agustincito1 <123766584+Agustincito1@users.noreply.github.com>
2024-09-15 21:05:41 -0300

    No hice muchos cambios pero bueno
    Intente poner footer y header en los modulos
    Me siento muy cansado :pensive:

--- Pagina/boletos.php
+++ Pagina/boletos.php
@@ -10,6 +10,39 @@
     <title>Boletos</title>
 </head>
 <body>
+    <header id="header">
+                <section class="header-section">
+                    <article class="h-s-article">
+                        <div class="h-s-a__div">
+                            <img class="h-s-a-d__img"src="imgs/iconoEmpresa.png" alt="" />
+                            
+                        </div>
+                        <div class="h-s-a__div">
+                            <h1 class="h-s-a-d__h1">AGENCIAS 42 y 43</h1>
+                            <h3 class="h-s-a-d__h3">Posadas Misiones</h3>
+                        </div>
+                    </article>
+                    <article >
+                        <nav class="h-s-a__nav">
+                            <li class="h-s-a-n-li"><a class="h-s-a-n-l__a" href="calendario.php">Calendario</a></li>
+                            <li class="h-s-a-n-li"><a class="h-s-a-n-l__a" href="">Nosotros</a></li>
+                            <li class="h-s-a-n-li"><a class="h-s-a-n-l__a" href="index.php#Empresa">Empresa</a></li>
+                            <li class="h-s-a-n-li"><a class="h-s-a-n-l__a" href="index.php">Viajar</a></li>
+                        </nav>
+                    </article>
+                </section>
+            </header>
+
+
+
+
+
+
+
+
+
+
+
 
     <?php 
         include "../libraries/functions.php";
@@ -152,6 +185,30 @@
         }
 
     ?>
+
+
+    <footer id="footer">
+                <section class="footer__section">
+                    <article class="f-s__article">
+                        <div class="f-s-a__div">
+                            
+                            <img class="f-s-a-d__img" src="imgs/iconoEmpresa.png" alt="" />
+                            
+                        </div>
+                        <div class="f-s-a__div">
+                            <h1 class="f-s-a-d__h1">AGENCIAS 42 y 43</h1>
+                            <h3 class="f-s-a-d__h3">Posadas Misiones</h3>
+                        </div>
+                    </article>
+                    <nav class="f-s__nav">
+                        <li><a class="f-s-n-l__a" href="">Principal</a></li>
+                        <li><a class="f-s-n-l__a" href="">Calendario</a></li>
+                        <li><a class="f-s-n-l__a" href="">Nosotros</a></li>
+                        <li><a class="f-s-n-l__a" href="">Contactanos</a></li>
+                    </nav>
+                </section>
+                <address class="footer__address"><a class="f_a__a" href="agustinlazari594@gmail.com">@agustinlazari594@gmail.com</a></address>
+        </footer>
 </body>
 </html>
 
