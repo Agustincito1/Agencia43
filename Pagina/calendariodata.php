b326d44b8667519a36143f77d79ba3f83549b246
Agustincito1 <123766584+Agustincito1@users.noreply.github.com>
2024-10-15 21:35:30 -0300

    actualizando el calendario

--- Pagina/calendariodata.php
+++ Pagina/calendariodata.php
@@ -1,9 +1,37 @@
 <?php
+    include "../libraries/functions.php";
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $dia = $_POST['eldia'];
         $mes =$_POST['elmes'];
         $diatxt = $_POST['deldiastr'];
         $anio = $_POST['elanio'];
+
+        
+        $query = "SELECT 
+            `Horario`, 
+            empresa.Nombre,
+            `Dia` 
+            
+        FROM `horario` 
+        INNER JOIN empresa on empresa.EmpresaID = horario.EmpresaID
+        WHERE horario.Dia = '$diatxt';";
+
+        $horarios = array();
+
+        if($datas = QueryAndGetData($query)){
+            while($data = mysqli_fetch_assoc($datas)){
+                $horarios[] =[
+                    'Horario' => $data['Horario'],
+                    'Empresa' => $data['Nombre']
+                ];
+                
+            }
+            echo json_encode($horarios);
+        }
+        else{
+            echo "Método no permitido.";
+        }
 
         
 
